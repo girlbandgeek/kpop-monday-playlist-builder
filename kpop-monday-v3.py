0a01c9bad14edfb352682e989c7d57943923409e
ec2-user <girlbandgeek@gmail.com>
2025-02-21 02:37:33 +0000

    Test more features with Mastodon.py

---
+++ kpop-monday-v3.py
@@ -0,0 +1,31 @@
+# foo
+from mastodon import Mastodon
+import time
+from dotenv import load_dotenv
+import os
+import random
+
+load_dotenv()
+
+mastodon = Mastodon(
+        client_id=os.getenv("Client_key"),
+        client_secret=os.getenv("Client_secret"),
+        access_token=os.getenv("access_token"),
+        api_base_url="https://mstdn.social"
+)
+
+'''
+# get my account id
+my_account = mastodon.account_verify_credentials()
+print(my_account.id)
+'''
+
+following=mastodon.account_following(113966619355975527)
+
+following_posts = []
+for i in following:
+    for posts in mastodon.account_statuses(i):
+        following_posts.append(posts.id)
+
+print(following_posts)
+
