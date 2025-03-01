4f36f2ed56091864754a40a1e4361dfbbe71598c
azureuser <girlbandgeek@gmail.com>
2025-03-01 05:32:13 +0000

    parse some individual keys from the status

--- kpop-monday-v3.py
+++ kpop-monday-v3.py
@@ -28,8 +28,11 @@ hashtag_dict = [(index, item) for index, item in enumerate(hashtag_posts)]
 hashtag_dict = dict(hashtag_dict)
 for key, value in hashtag_dict.items():
     print(f"{key}:")
-    print(f"account id: ", hashtag_dict[key]["id"])
+    # print(f"account id: ", hashtag_dict[key]["id"])
+    print(f"account id: ", hashtag_dict[key]["account"]["id"])
+    print(f"account username: ", hashtag_dict[key]["account"]["username"])
     print(f"account name: ", hashtag_dict[key]["account"]["acct"])
+    print(f"content: ", hashtag_dict[key]["content"])
     for kkey in hashtag_dict[key].keys():
         # Print each key and the value in the status
         print(f"{kkey}: ", hashtag_dict[key][kkey])
