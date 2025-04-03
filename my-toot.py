4ec4acad78f385b2e4c9f154da5126154db46c41
Daniel Oxenhandler <37917480+girlbandgeek@users.noreply.github.com>
2025-04-03 12:49:05 -0700

    Create my-toot.py
    Add test script to post status

---
+++ my-toot.py
@@ -0,0 +1,27 @@
+# Write some code to post a status on our timeline
+from mastodon import Mastodon
+import datetime
+from datetime import date
+from datetime import timedelta
+from datetime import time
+from dateutil import parser
+from dotenv import load_dotenv
+import os
+import sys
+import re
+
+#from datetime import datetime, date, time, timezone
+
+
+load_dotenv()
+
+mastodon = Mastodon(
+        client_id=os.getenv("Client_key"),
+        client_secret=os.getenv("Client_secret"),
+        access_token=os.getenv("access_token"),
+        api_base_url="https://mstdn.social"
+)
+
+status_text = 'This is a test post'
+
+mastodon.status_post(status = status_text, visibility = "public")
