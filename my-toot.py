e5d9c49708d4314d8b9dc771d2e7dc9079e76e92
azureuser <girlbandgeek@gmail.com>
2025-04-03 20:12:16 +0000

    MVP for toot function

--- my-toot.py
+++ my-toot.py
@@ -22,6 +22,19 @@ mastodon = Mastodon(
         api_base_url="https://mstdn.social"
 )
 
-status_text = 'This is a test post'
+stp1 = 'Hi! Here is the KpopMonday compilation playlist for December 2, 2024.'
+stp2 = 'This week\'s theme is #TwoSongsOneTitle'
+stp3 = 'Total number of toots for  TwoSongsOneTitle is:  77'
+stp4 = 'Top contributor(s) this week with 31 toots:  Erzbet@apobangpo.space'
+stp5 = 'https://youtube.com/playlist?list=PLZyP4b181sFYgqLdKYRNzFhWVx1_iYWwp&si=zMl2upkP9OV34UJu'
+stp6 = '#fake_tag'
+'''
+x = ' '.join(("multiline String ",
+			"Python Language",
+			"Welcome to GFG"))
+print(x)
+'''
+status_text = '\n\n'.join((stp1, stp2, stp3, stp4, stp5, stp6))
+print(status_text)
 
 mastodon.status_post(status = status_text, visibility = "public")
