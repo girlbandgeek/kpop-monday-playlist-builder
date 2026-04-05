fc6ea63f0d977213f670747546ed1f2712b14ec3
daniel <girlbandgeek@gmail.com>
2026-04-05 13:40:14 -0700

    clean up looping in parser_test

--- Test/parser_test.py
+++ Test/parser_test.py
@@ -6,9 +6,6 @@ import re
 # ccontent = hashtag_dict[key]["content"]
 my_content = ['<p>I love when kpop does Spanish, and this one is one of my absolute favs</p><p>Demente - Chungha</p><p><a href="https://www.youtube.com/watch?si=6EQBevEEr1S_H7EH&amp;v=KsHZI6nYLFM" rel="nofollow noopener" translate="no" target="_blank"><span class="invisible">https://www.</span><span class="ellipsis">youtube.com/watch?si=6EQBevEEr</span><span class="invisible">1S_H7EH&amp;v=KsHZI6nYLFM</span></a></p><p><a href="https://kpop.social/tags/KpopMonday" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>KpopMonday</span></a> <a href="https://kpop.social/tags/NonKoreanReleases" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>NonKoreanReleases</span></a> <a href="https://kpop.social/tags/kpop" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>kpop</span></a> <a href="https://kpop.social/tags/Chungha" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>Chungha</span></a></p>', '<p><a href="https://borahae.love/tags/nonkoreanreleases" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>nonkoreanreleases</span></a> <a href="https://borahae.love/tags/kpopmonday" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>kpopmonday</span></a> </p><p>No official video, but <a href="https://borahae.love/tags/BTS" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>BTS</span></a> <a href="https://borahae.love/tags/dontleaveme" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>dontleaveme</span></a> from Japanese album <a href="https://borahae.love/tags/FaceYourself" class="mention hashtag" rel="nofollow noopener" target="_blank">#<span>FaceYourself</span></a></p><p><a href="https://youtu.be/8Fzc14ftwN4?si=JOrVdauQRWVFo8r0" rel="nofollow noopener" translate="no" target="_blank"><span class="invisible">https://</span><span class="ellipsis">youtu.be/8Fzc14ftwN4?si=JOrVda</span><span class="invisible">uQRWVFo8r0</span></a></p>']
 
-for ccontent in my_content:
-    print(ccontent)
-    print('')
     # match = re.findall(r'(m\.youtube\.com/watch\?v=|www\.youtube\.com/watch\?v=|www\.youtube\.com/v/|youtu\.be/)([\w-]+)', ccontent)
     # match = re.findall(r'(m\.youtube\.com/watch\?v=|www\.youtube\.com/watch\?v=|www\.youtube\.com/v/|youtu\.be/|www\.youtube\.com/watch\?si=)([\w-]+)', ccontent)
 
@@ -28,34 +25,38 @@ for ccontent in my_content:
 	#r'www\.youtube\.com/watch\?si=[0-9A-Za-z_-]{11}'
 #	])
 
-    vPattern = '|'.join([
-	# Normal watch URL   :: r'[0-9A-Za-z_-]{11}',
-	r'/www\.youtube\.com/watch(_popup)?(\.php)?/?\?(\S*&)?v=',
-	r'm\.youtube\.com/watch\?v=',
-	r'www\.youtube\.com/watch\?v=',
-	r'www\.youtube\.com/v/',
-	r'youtu\.be/',
-	r'www\.youtube\.com/watch\?si='
-	])
+vPattern = '|'.join([
+# Normal watch URL   :: r'[0-9A-Za-z_-]{11}',
+r'/www\.youtube\.com/watch(_popup)?(\.php)?/?\?(\S*&)?v=',
+r'm\.youtube\.com/watch\?v=',
+r'www\.youtube\.com/watch\?v=',
+r'www\.youtube\.com/v/',
+r'youtu\.be/',
+r'www\.youtube\.com/watch\?si='
+])
 
-    print("vPattern: ")
-    print(vPattern)
-    print()
-    borgle = '([\\w-]+)'
-    print("borgle: ")
-    print(borgle)
-    print()
-    videoPattern = vPattern + borgle
-    print("videoPattern: ")
-    print(videoPattern)
-    print()
+print("vPattern: ")
+print(vPattern)
+print()
+borgle = '([\\w-]+)'
+print("borgle: ")
+print(borgle)
+print()
+videoPattern = vPattern + borgle
+print("videoPattern: ")
+print(videoPattern)
+print()
 
-    pattern = re.compile(videoPattern)
-    print(pattern)
+pattern = re.compile(videoPattern)
+print(pattern)
+
+for ccontent in my_content:
+    print(ccontent)
+    print('')
 
     match = pattern.findall(ccontent)
-
     if len(match) > 0:
+
         print(f"videos: ", match)
         vid_list=[]
         for iitem in match:
